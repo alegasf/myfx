d6316af457d8c2dc369a733c136851fa42ce0f88
”Alexey <4alegasf@gmail.com>
2022-05-12 14:31:33 +0500

    db pass changed

--- src/main/java/com/example/myfx/Configs.java
+++ src/main/java/com/example/myfx/Configs.java
@@ -7,6 +7,7 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "SQLpassword1";
+    protected String dbPass = "";
+//  protected String dbPass2 = "SQLpassword1";
     protected String dbName = "users";
 }
